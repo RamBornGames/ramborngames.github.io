877758b4aa156fd1e22602f2afb84345c0a5cba8
Charlie Feuerborn <cfireborn@gmail.com>
2026-08-27 13:58:49 -0700

    Updated to aug 27 build 1

--- compersion/ServiceWorker.js
+++ compersion/ServiceWorker.js
@@ -1,13 +1,13 @@
-const version = encodeURIComponent("2026.08.19.4-dd00f7dc");
+const version = encodeURIComponent("2026.08.27.1-4fe748e1");
 const cachePrefix = "unity-webgl-" + self.registration.scope + "-";
 const legacyCachePrefix = "Ramsey Fireborn Games Studio-Compersion-";
 const cacheName = cachePrefix + version;
 const contentToCache = [
     "index.html",
-    "Build/2026.08.19_build3_compersion2d.loader.js?v=" + version,
-    "Build/2026.08.19_build3_compersion2d.framework.js.unityweb?v=" + version,
-    "Build/2026.08.19_build3_compersion2d.data.unityweb?v=" + version,
-    "Build/2026.08.19_build3_compersion2d.wasm.unityweb?v=" + version,
+    "Build/2026.08.27_build1_compersion2d.loader.js?v=" + version,
+    "Build/2026.08.27_build1_compersion2d.framework.js.unityweb?v=" + version,
+    "Build/2026.08.27_build1_compersion2d.data.unityweb?v=" + version,
+    "Build/2026.08.27_build1_compersion2d.wasm.unityweb?v=" + version,
     "TemplateData/style.css?v=" + version,
     "manifest.webmanifest?v=" + version,
     "TemplateData/compersion-favicon-32.png?v=" + version,
